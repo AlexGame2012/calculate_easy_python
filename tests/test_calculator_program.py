9397648bd5541535e79f71d4c348abbe4d573fdb
AlexStudio Code <139368285+AlexGame2012@users.noreply.github.com>
2026-03-15 23:18:05 +0700

    Update test_calculator_program.py

--- tests/test_calculator_program.py
+++ tests/test_calculator_program.py
@@ -4,16 +4,16 @@ from calculate.calculator_program import calculate
 def test_calculate_addition():
     assert calculate(1, 1, '+') == 2
 
-def test_calculate_division():
+def test_calculate_division2():
     assert calculate(8, 2, '/') == 4
 
-def test_calculate_division():
+def test_calculate_division3():
     assert calculate(3, 4, '*') == 12
 
-def test_calculate_division():
+def test_calculate_division4():
     assert calculate(1, 12, '-') == -11
 
-def test_calculate_division():
+def test_calculate_division5():
     assert calculate(4, 0, '/') == "Ошибка: Деление на ноль."
 
 def test_calculate_unknown_operation():
